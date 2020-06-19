f4bcde818496d181c9b16bfef07044e44ebf24e7
sarvesh2124 <67094722+sarvesh2124@users.noreply.github.com>
2020-06-19 18:51:53 +0530

    check email and signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CheckEmailComponent } from './check-email/check-email.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sign-up',component: SignUpComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'check-email', component: CheckEmailComponent},
+  {path: '**', component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CheckEmailComponent } from './check-email/check-email.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    DashboardComponent,
+    CheckEmailComponent
   ],
   imports: [
     BrowserModule,
